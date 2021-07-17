c8e220f0449335715e1165b53efd70cde198d460
Nandan2411 <71706233+Nandan2411@users.noreply.github.com>
2021-07-17 13:10:54 +0530

    Add files via upload

---
+++ web lab/Setmap.js
@@ -0,0 +1,3 @@
+let capitals = new Map()
+capitals.set('bangalore', 'KA')
+console.log(capitals.get('bangalore'))
---
+++ web lab/validateemail.js
@@ -0,0 +1,5 @@
+function validateEmail(elementValue) {
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(elementValue);
+}
+console.log(validateEmail('adarsh@gmail.com'))
